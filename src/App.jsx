e5bf7512a26a187d2018de7b5a701d56eb91194e
YienYang <chooyienyang2003@gmail.com>
2025-03-10 23:05:55 +0800

    1

--- src/App.jsx
+++ src/App.jsx
@@ -22,21 +22,19 @@ function App() {
   }
 
   return (
-    <Background />
-    // <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base'>
-    //   <Background />
-    //   <Hero />
-    //   <Generator
-    //     poison={poison}
-    //     setPoison={setPoison}
-    //     muscles={muscles}
-    //     setMuscles={setMuscles}
-    //     goal={goal}
-    //     setGoal={setGoal}
-    //     updateWorkout={updateWorkout}
-    //   />
-    //   {workout && (<Workout workout={workout} />)}
-    // </main>
+    <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base'>
+      <Hero />
+      <Generator
+        poison={poison}
+        setPoison={setPoison}
+        muscles={muscles}
+        setMuscles={setMuscles}
+        goal={goal}
+        setGoal={setGoal}
+        updateWorkout={updateWorkout}
+      />
+      {workout && (<Workout workout={workout} />)}
+    </main>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,21 +3,21 @@ import Button from "./Button";
 
 export default function Hero() {
   return (
-    <div className="min-h-scree flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4">
+    <div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4">
       <div className="flex flex-col gap-4">
-        <p>IT'S TIME TO GET</p>
+        <p>PREPARE TO EMBRACE</p>
         <h1 className="uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
-          Swole<span className="text-blue-400 font-medium">normous</span>
+          Ultimate<span className="text-blue-400 font-medium">Ascension</span>
         </h1>
       </div>
       <p className="text-sm md:text-base font-light">
-        I hereby acknowledgement that I may become{" "}
+        By proceeding, I fully accept the possibility of{" "}
         <span className="text-blue-400 font-medium">
-          unbelievably swolenormous
+          unparalleled transformation
         </span>{" "}
-        and accept all risks of becoming the local{" "}
-        <span className="text-blue-400 font-medium">mass montrosity</span>,
-        afflicted with severe body dismorphia, unable to fit through doors.
+        and acknowledge all consequences of evolving into a{" "}
+        <span className="text-blue-400 font-medium">limitless powerhouse</span>,
+        forever altering my perception of reality and physical boundaries.
       </p>
       <Button
         func={() => {
